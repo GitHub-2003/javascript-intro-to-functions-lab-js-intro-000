957dec326c73d83b535adfd58b48c754704ab541
MJU <marie.ju2012@outlook.com>
2020-03-03 21:51:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,15 +10,6 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  if string === string.toLowerCase() {
-    return "I can't hear you!"
-  } else if string === string.toUpperCase() {
-    return "YES INDEED!"
-  } else {
-      if string === "I love you, Grandma." {
-        return "I love you, too."
-      }
-
 }
 
 function logWhisper(string) {
